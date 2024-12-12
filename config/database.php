7f769057feae1112a3be3fdb8df03fe47431d0ad
root <root@vm-expeprd-aleph-prod-001.e1kf3ie2uzvu5bqmncvd5ewrzh.cx.internal.cloudapp.net>
2024-12-12 16:26:46 +0000

    cambios integrados con ngix en el server de ubuntu

--- config/database.php
+++ config/database.php
@@ -46,11 +46,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'deveximio.mysql.database.azure.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'aleph'),
+            'username' => env('DB_USERNAME', 'aleph'),
+            'password' => env('DB_PASSWORD', '3ximio@2024$'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -59,7 +59,9 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+PDO::MYSQL_ATTR_SSL_CA => env('DB_SSL_CERT'),
+        PDO::ATTR_TIMEOUT => 600,
+/*                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),*/
             ]) : [],
         ],
 
